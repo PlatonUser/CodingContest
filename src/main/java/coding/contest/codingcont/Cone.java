29ca5786c3b5308a8bfeda6953ad7cdb12c7fce1
snezhv <snizhana.pashkevych@students.fhnw.ch>
2025-04-29 15:20:00 +0200

    added cone class code

--- src/main/java/coding/contest/codingcont/Cone.java
+++ src/main/java/coding/contest/codingcont/Cone.java
@@ -1,5 +1,39 @@
 package coding.contest.codingcont;
 
-public class Cone {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cone {
+    private final LocalDate date;
+    private final List<Scoop> scoops = new ArrayList<>();
+
+    public Cone() {
+        this.date = LocalDate.now();
+    }
+
+    public boolean addScoop(Scoop scoop) {
+        if (scoops.size() < 3) {
+            scoops.add(scoop);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Scoop> getScoops() {
+        return scoops;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getTotalGrossPrice() {
+        return scoops.stream().mapToDouble(Scoop::getGrossPrice).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "Cone (" + date + ")";
+    }
 }
