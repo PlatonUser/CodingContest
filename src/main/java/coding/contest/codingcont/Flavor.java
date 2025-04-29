72de38a42c6d37766fadd526ca57f67fe743d43e
PlatonUser <platonpashkevich31@gmail.com>
2025-04-29 15:29:31 +0200

    pushing flavor

--- src/main/java/coding/contest/codingcont/Flavor.java
+++ src/main/java/coding/contest/codingcont/Flavor.java
@@ -1,28 +1,4 @@
 package coding.contest.codingcont;
 
-public enum Flavor {
-    CHOCOLATE("Chocolate", 2.50),
-    VANILLA("Vanilla", 2.30),
-    STRAWBERRY("Strawberry", 2.40),
-
-    private final String name;
-    private final double grossPrice;
-
-    Flavor(String name, double grossPrice) {
-        this.name = name;
-        this.grossPrice = grossPrice;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getGrossPrice() {
-        return grossPrice;
-    }
-
-    @Override
-    public String toString() {
-        return name + " (€" + grossPrice + ")";
-    }
+public class Flavor {
 }
