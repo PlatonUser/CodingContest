017eb077eb4438033ebf5211918e4e347c16053a
snezhv <snizhana.pashkevych@students.fhnw.ch>
2025-04-29 15:33:53 +0200

    added main code

--- src/main/java/coding/contest/codingcont/main.java
+++ src/main/java/coding/contest/codingcont/main.java
@@ -1,4 +1,20 @@
 package coding.contest.codingcont;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main {
+
+public class main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/view/IceCreamView.fxml")));
+        stage.setTitle("Ice Cream Cashier");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
