c072835ceb560c0a899294b69266b1ee1d09bd16
snezhv <snizhana.pashkevych@students.fhnw.ch>
2025-04-29 15:29:58 +0200

    added cone class code

--- src/main/java/coding/contest/codingcont/Flavor.java
+++ src/main/java/coding/contest/codingcont/Flavor.java
@@ -3,7 +3,7 @@ package coding.contest.codingcont;
 public enum Flavor {
     CHOCOLATE("Chocolate", 2.50),
     VANILLA("Vanilla", 2.30),
-    STRAWBERRY("Strawberry", 2.40),
+    STRAWBERRY("Strawberry", 2.40);
 
     private final String name;
     private final double grossPrice;
